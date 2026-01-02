404ba1d275f701454245ca4f23211408741d3d6c
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-01-02 16:13:57 +0000

    Attempt to add fog control & AxolotlClient config integration

---
+++ src/main/java/net/eliotex/zirconium/Zirconium.java
@@ -0,0 +1,62 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * For more information, see the LICENSE file.
+ */
+
+package net.eliotex.zirconium;
+
+import net.eliotex.zirconium.config.ZirconiumConfig;
+import net.fabricmc.loader.api.FabricLoader;
+import net.ornithemc.osl.entrypoints.api.client.ClientModInitializer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zirconium implements ClientModInitializer {
+
+	public static final String MODID = "zirconium";
+	public static boolean AXOLOTLCLIENT;
+
+	private static Zirconium instance;
+
+	// Since AxolotlClient may initialize this class as a module before it gets loaded as a mod by fabric we have to defer the former to run after the latter.
+	// But since the load order is non-deterministic this may not always be the case
+	private static boolean loadedByFabric;
+	private static final List<Runnable> tasks = new ArrayList<>();
+
+	public Zirconium() {
+		if (instance != null) {
+			throw new IllegalStateException();
+		}
+
+		loadedByFabric = true;
+		instance = this;
+		tasks.forEach(Runnable::run);
+		tasks.clear();
+	}
+
+	public static void runAfterFabricLoad(Runnable task) {
+		if (loadedByFabric) {
+			task.run();
+		} else tasks.add(task);
+	}
+
+	@Override
+	public void initClient() {
+		ZirconiumConfig.instance.initConfig();
+		AXOLOTLCLIENT = FabricLoader.getInstance().isModLoaded("axolotlclient");
+	}
+}
---
+++ src/main/java/net/eliotex/zirconium/mixin/fogcontrol/GameRendererMixin.java
@@ -0,0 +1,32 @@
+package jp.s12kuma01.celeritasextra.mixin.render.fog;
+
+import net.minecraft.client.render.GameRenderer;
+import net.eliotex.zirconium.config.ZirconiumConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Simple fog control
+ * In 1.12.2, fog is controlled in EntityRenderer's setupFog methods
+ * This is a simplified version - full fog control would be more complex
+ */
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+
+    /**
+     * Control fog rendering (simplified version)
+     * This cancels the fog setup, effectively disabling fog
+     */
+    @Inject(
+        method = "renderFog",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void renderFog(int mode, float tickDelta, CallbackInfo ci) {
+        if (!ZirconiumConfig.instance.showFog.get()) {
+            ci.cancel();
+        }
+    }
+}
